380672cdc1945ea97c255827ce939ba2091489e0
Your Name <you@example.com>
2023-03-29 13:57:39 -0300

    finalização

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,19 @@
 import { Request, Response } from "express";
+import { Pet } from "../models/pet";
+import { createMenuObject } from "../helpers/createMenuObject";
 
 export const search = (req: Request, res: Response) => {
+    let query: string = req.query.search as string;
+    let list = Pet.getFromName(query);
 
-    // res.render('pages/search');
+    if(!query) {
+        res.redirect('/');
+        return
+    }
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    });
 };
